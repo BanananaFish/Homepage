3cf25d3e446844b5c8eaee8e8d0d466c5017d540
张浩 <BananaFish@Titanium.local>
2018-10-22 21:56:25 +0800

    login page update1

--- File/login.js
+++ File/login.js
@@ -3,9 +3,11 @@ var x = document.getElementById('date');
 x.innerText = String(now);
 
 function checkform(){
-    let username = document.getElementById('username');
-    let password = document.getElementById('password');
-    if ((username === "Bananafish") && (password === "123456")){
+    let username = document.getElementsByTagName('input')[0];
+    let password = document.getElementsByTagName('input')[1];
+    console.log(username.value);
+    
+    if ((username.value === "Bananafish") && (password.value === "123456")){
         alert("Success!");
         return true;
     }else{
